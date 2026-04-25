a1da1d76f4dd3f1d2aeaceb54b8bcfa74fd6e60a
himakshi0510 <himakshibansal6@gmail.com>
2026-04-25 21:40:28 +0530

    rock-paper-scissor game

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ while True:
     if choice==1:
         print("Starting the game...")
         game()
-    elif choice == 2:
+    elif choice == 2:1
         print("Exiting the game...")
         break
     else:
